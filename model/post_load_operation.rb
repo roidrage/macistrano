49d47dc7f4f8cc613c230173c573f0e9b88f1994
Mathias Meyer <meyer@paperplanes.de>
2008-06-22 22:39:40 +0200

    added slightly higher timeout for post operations

--- model/post_load_operation.rb
+++ model/post_load_operation.rb
@@ -20,6 +20,7 @@ class PostLoadOperation < LoadOperation
     request.setHTTPMethod("POST")
     request.setValue_forHTTPHeaderField(encoded_body.length.to_s, "Content-Length")
     request.setValue_forHTTPHeaderField("text/xml", "Content-Type")
+    request.setTimeoutInterval(15.0)
     
     request.setHTTPBody encoded_body
     
