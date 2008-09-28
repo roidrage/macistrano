98c493b29a110f2950658ee2490889c016d93bfb
Mathias Meyer <meyer@paperplanes.de>
2008-09-28 17:56:55 +0200

    more tests

--- spec/project_controller_spec.rb
+++ spec/project_controller_spec.rb
@@ -1,7 +1,5 @@
 require File.join(File.dirname(__FILE__), '/spec_helper')
 
-require 'rubygems'
-gem 'rspec'
 require 'project_controller'
 
 describe ProjectController, "when notified of a running build" do
@@ -11,8 +9,13 @@ describe ProjectController, "when notified of a running build" do
     @controller.stub!(:set_status_icon)
   end
   
-  it "should set up a timer to fetch the deployment status"
+  it "should set up a timer to fetch the deployment status" do
+    @controller.should_receive(:set_stage_submenu_enabled).with(anything, false, an_instance_of(String))
+    @controller.build_running stub(:notification, :object => nil)
+  end
   
   it "should set the status icon to running" do
+    @controller.should_receive(:set_status_icon).with("success-building")
+    @controller.build_running stub(:notification, :object => nil)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 gem 'rspec'
+require 'spec'
+require 'spec/mocks'
 
 $:.unshift File.join(File.dirname(__FILE__), '../model')
 $:.unshift File.join(File.dirname(__FILE__), '../controller')
